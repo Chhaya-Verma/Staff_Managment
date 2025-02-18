5561789b01bb8f4ba4a6d4c4f75076d9d0706996
Ina Dewangan <inadewangan14@gmail.com>
2025-02-18 10:25:41 +0530

    Commit the few minner changes for pull request

--- src/SupervisorDashboard/SupervisorDashboard.js
+++ src/SupervisorDashboard/SupervisorDashboard.js
@@ -1,13 +1,28 @@
-import React from 'react';
+// import React from 'react';
 
-function SupervisorDashboard() {
+// function SupervisorDashboard() {
+//   return (
+//     <div>
+//       <h1>Welcome Supervisor</h1>
+//       <p>This is the Supervisor page</p>
+//       {/* Render Supervisor-specific content */}
+//     </div>
+//   );
+// }
+
+// export default SupervisorDashboard;
+
+
+// src/pages/SupervisorDashboard.js
+import React from "react";
+import SupervisorLayout from "./SupervisorLayout";
+
+const SupervisorDashboard = () => {
   return (
-    <div>
-      <h1>Welcome Supervisor</h1>
-      <p>This is the Supervisor page</p>
-      {/* Render Supervisor-specific content */}
-    </div>
+    <>
+    <SupervisorLayout />
+    </>
   );
-}
+};
 
 export default SupervisorDashboard;
